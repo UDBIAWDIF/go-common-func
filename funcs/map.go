1da55dd832bf4c4017e50b38873c1357ca0d231d
UID <UID@YEAH.NET>
2024-03-12 16:56:15 +0800

    [feat]添加 map 处理函数

--- funcs/map.go
+++ funcs/map.go
@@ -21,7 +21,7 @@ func MapToOrderedMap(input map[string]interface{}) *orderedmap.OrderedMap {
 }
 
 // 结构体转换成MAP
-func StructToMap(input interface{}) map[string]interface{} {
+func StructToStringMap(input interface{}) map[string]interface{} {
 	var output map[string]interface{}
 	mapstructure.Decode(input, &output)
 	return output
@@ -29,5 +29,5 @@ func StructToMap(input interface{}) map[string]interface{} {
 
 // 结构体转换成有序MAP(顺手按KEY升序排序)
 func StructToOrderedMap(input interface{}) *orderedmap.OrderedMap {
-	return MapToOrderedMap(StructToMap(input))
+	return MapToOrderedMap(StructToStringMap(input))
 }
