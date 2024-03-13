c7051346ea43616b97ff488c87e0157418b8fdc1
UID <UID@YEAH.NET>
2024-03-13 08:30:14 +0800

    [feat]httppost

--- funcs/http.go
+++ funcs/http.go
@@ -1,39 +1,46 @@
 package funcs
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strings"
+	"time"
 )
+
+const HTTP_TIMEOUT_DEFAULT time.Duration = time.Second * 10
+const HTTP_CONTENT_TYPE_JSON string = "application/json"
 
 func HttpGet(url string) (string, error) {
 	runnable := true
-	var body []byte
+	var content []byte
 	var err error
+	client := http.Client{Timeout: HTTP_TIMEOUT_DEFAULT}
 
 	var response *http.Response
-	response, err = http.Get(url)
+	response, err = client.Get(url)
 	if err != nil {
 		runnable = false
 	}
 	defer response.Body.Close()
 
 	if runnable {
-		body, err = ioutil.ReadAll(response.Body)
+		content, err = io.ReadAll(response.Body)
 		if err != nil {
 			runnable = false
 		}
 	}
 
-	return string(body), err
+	return string(content), err
 }
 
 func HttpGetHttpCode(url string) (int, error) {
 	runnable := true
 	var code int
 	var err error
+	client := http.Client{Timeout: HTTP_TIMEOUT_DEFAULT}
 
 	var response *http.Response
-	response, err = http.Get(url)
+	response, err = client.Get(url)
 	if err != nil {
 		runnable = false
 	}
@@ -44,4 +51,27 @@ func HttpGetHttpCode(url string) (int, error) {
 	}
 
 	return code, err
+}
+
+func HttpPost(url string, contentType string, body []byte) (string, error) {
+	runnable := true
+	var content []byte
+	var err error
+	client := http.Client{Timeout: HTTP_TIMEOUT_DEFAULT}
+
+	var response *http.Response
+	response, err = client.Post(url, contentType, strings.NewReader(string(body)))
+	if err != nil {
+		runnable = false
+	}
+	defer response.Body.Close()
+
+	if runnable {
+		content, err = io.ReadAll(response.Body)
+		if err != nil {
+			runnable = false
+		}
+	}
+
+	return string(content), err
 }
