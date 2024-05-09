7a282120d7201c663935d40c946a96de80c90d9c
UID <UID@YEAH.NET>
2024-05-09 18:10:46 +0800

    [feat]添加方法: 字符串驼峰转下划线

--- funcs/string.go
+++ funcs/string.go
@@ -2,6 +2,7 @@ package funcs
 
 import (
 	"encoding/json"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -9,6 +10,10 @@ import (
 
 	"github.com/axgle/mahonia"
 )
+
+var MATCH_NON_ALPHA_NUMERIC = regexp.MustCompile(`[^a-zA-Z0-9]+`)
+var MATCH_FIRST_CAP = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var MATCH_ALL_CAP = regexp.MustCompile("([a-z0-9])([A-Z])")
 
 func StringToInt64(e string) (int64, error) {
 	return strconv.ParseInt(e, 10, 64)
@@ -88,4 +93,12 @@ func ConvertGBKToUTF8(src string, srcCode string, tagCode string) string {
 	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
 	result := string(cdata)
 	return result
+}
+
+// 驼峰转下划线
+func StrToSnakeCase(str string) string {
+	str = MATCH_NON_ALPHA_NUMERIC.ReplaceAllString(str, "_")    //非常规字符转化为 _
+	snake := MATCH_FIRST_CAP.ReplaceAllString(str, "${1}_${2}") //拆分出连续大写
+	snake = MATCH_ALL_CAP.ReplaceAllString(snake, "${1}_${2}")  //拆分单词
+	return strings.ToLower(snake)                               //全部转小写
 }
