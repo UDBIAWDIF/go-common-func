658d017901303d500c16cba2d5a3965613815375
UID <UID@YEAH.NET>
2024-05-18 08:35:10 +0800

    [fix]Http请求有BUG, 如果请求地址不存在, 响应体是nil, 不能关闭, 要判断不是nil才关闭

--- funcs/http.go
+++ funcs/http.go
@@ -21,13 +21,16 @@ func HttpGet(url string) (string, error) {
 	if err != nil {
 		runnable = false
 	}
-	defer response.Body.Close()
 
 	if runnable {
 		content, err = io.ReadAll(response.Body)
 		if err != nil {
 			runnable = false
 		}
+	}
+
+	if response != nil {
+		response.Body.Close()
 	}
 
 	return string(content), err
@@ -44,10 +47,13 @@ func HttpGetHttpCode(url string) (int, error) {
 	if err != nil {
 		runnable = false
 	}
-	defer response.Body.Close()
 
 	if runnable {
 		code = response.StatusCode
+	}
+
+	if response != nil {
+		response.Body.Close()
 	}
 
 	return code, err
@@ -64,13 +70,56 @@ func HttpPost(url string, contentType string, body []byte) (string, error) {
 	if err != nil {
 		runnable = false
 	}
-	defer response.Body.Close()
 
 	if runnable {
 		content, err = io.ReadAll(response.Body)
 		if err != nil {
 			runnable = false
 		}
+	}
+
+	if response != nil {
+		response.Body.Close()
+	}
+
+	return string(content), err
+}
+
+type HttpRequestHandler func(*http.Request)
+
+// 发启HTTP请求, 可以使用请求处理器对请求进行一些初始化操作, 比如设置头部信息等
+func HttpPostUseHandle(url string, body []byte, httpRequestHandler HttpRequestHandler) (string, error) {
+	runnable := true
+	var content []byte
+	var err error
+	client := http.Client{Timeout: HTTP_TIMEOUT_DEFAULT}
+	var response *http.Response
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(string(body)))
+	if err != nil {
+		runnable = false
+	}
+
+	if runnable {
+		if httpRequestHandler != nil {
+			httpRequestHandler(req)
+		}
+
+		response, err = client.Do(req)
+		if err != nil {
+			runnable = false
+		}
+	}
+
+	if runnable {
+		content, err = io.ReadAll(response.Body)
+		if err != nil {
+			runnable = false
+		}
+	}
+
+	if response != nil {
+		response.Body.Close()
 	}
 
 	return string(content), err
