a8ae9a60390b988a370db7153c66a95318ad76c0
UID <UID@YEAH.NET>
2024-05-25 16:03:00 +0800

    [feat]反射工具: 获取指定属性值; 对象属性复制

---
+++ funcs/reflect.go
@@ -0,0 +1,61 @@
+package funcs
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
+
+// 获取指定属性的值
+func ReflectGetFieldValue[T any](data interface{}, fieldName string) T {
+	dataValue := reflect.ValueOf(data)
+	return dataValue.FieldByName(fieldName).Interface().(T)
+}
+
+// 属性复制
+func ReflectCopyProperties(dst, src interface{}) (err error) {
+	// 防止意外panic
+	defer func() {
+		if e := recover(); e != nil {
+			err = fmt.Errorf("%v", e)
+		}
+	}()
+
+	dstType, dstValue := reflect.TypeOf(dst), reflect.ValueOf(dst)
+	srcType, srcValue := reflect.TypeOf(src), reflect.ValueOf(src)
+
+	// dst必须结构体指针类型
+	if dstType.Kind() != reflect.Ptr || dstType.Elem().Kind() != reflect.Struct {
+		return errors.New("dst type should be a struct pointer")
+	}
+
+	// src必须为结构体或者结构体指针
+	if srcType.Kind() == reflect.Ptr {
+		srcType, srcValue = srcType.Elem(), srcValue.Elem()
+	}
+	if srcType.Kind() != reflect.Struct {
+		return errors.New("src type should be a struct or a struct pointer")
+	}
+
+	// 取具体内容
+	dstType, dstValue = dstType.Elem(), dstValue.Elem()
+
+	// 属性个数
+	fieldCount := dstType.NumField()
+	for fieldIdx := 0; fieldIdx < fieldCount; fieldIdx++ {
+		// 属性
+		curField := dstType.Field(fieldIdx)
+		// 待填充属性值
+		curValue := srcValue.FieldByName(curField.Name)
+		// 无效，说明src没有这个属性 || 属性同名但类型不同
+		if !curValue.IsValid() || curField.Type != curValue.Type() {
+			continue
+		}
+
+		if dstValue.Field(fieldIdx).CanSet() {
+			dstValue.Field(fieldIdx).Set(curValue)
+		}
+	}
+
+	return nil
+}
---
+++ funcs/reflect_test.go
@@ -0,0 +1,46 @@
+package funcs
+
+import (
+	"testing"
+	"time"
+)
+
+type UserForTest struct {
+	Name     string
+	Birthday *time.Time
+	Nickname string
+	Role     string
+	Age      int32
+	FakeAge  *int32
+}
+
+type EmployeeForTest struct {
+	Name      string
+	Birthday  *time.Time
+	NickName  *string
+	Age       int64
+	FakeAge   int
+	EmployeID int64
+	DoubleAge int32
+	SuperRule string
+}
+
+// go test -v -run="TestReflectCopyProperties"
+func TestReflectCopyProperties(t *testing.T) {
+	nickName := "付晓南老公"
+	birthday, _ := StrToLocalTime("1979-01-01")
+	em := EmployeeForTest{
+		Name:     "uid",
+		NickName: &nickName,
+		Age:      18,
+		FakeAge:  20,
+		Birthday: &birthday,
+	}
+	user := UserForTest{}
+
+	err := ReflectCopyProperties(&user, em)
+	if err != nil {
+		t.Log(err.Error())
+	}
+	t.Log(user)
+}
